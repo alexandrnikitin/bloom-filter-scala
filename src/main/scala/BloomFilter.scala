0a885e3449e37b35261d583acbcc840b7b3e2306
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-02-07 22:56:42 +0200

    Draft API

--- src/main/scala/BloomFilter.scala
+++ src/main/scala/BloomFilter.scala
@@ -1,3 +1,34 @@
-object BloomFilter {
+class BloomFilter[T](numberOfBits: Long, numberOfHashes: Int) {
 
+  def add(x: T): Unit = {
+  }
+
+  def checkAndAdd(x: T): Boolean = {
+    false
+  }
+
+  def mightContain(x: T): Boolean = {
+    false
+  }
+
+  def expectedFalsePositiveRate(): Double = {
+    0
+  }
+
+}
+
+object BloomFilter {
+  def apply[T](numberOfItems: Long, falsePositiveRate: Double): BloomFilter[T] = {
+    val nb = optimalNumberOfBits(numberOfItems, falsePositiveRate)
+    val nh = optimalNumberOfHashes(numberOfItems, nb)
+    new BloomFilter[T](nb, nh)
+  }
+
+  def optimalNumberOfBits(numberOfItems: Long, falsePositiveRate: Double): Long = {
+    math.ceil(-1 * numberOfItems * math.log(falsePositiveRate) / math.log(2) / math.log(2)).toLong
+  }
+
+  def optimalNumberOfHashes(numberOfItems: Long, numberOfBits: Long): Int = {
+    math.ceil(numberOfBits / numberOfItems * math.log(2)).toInt
+  }
 }
