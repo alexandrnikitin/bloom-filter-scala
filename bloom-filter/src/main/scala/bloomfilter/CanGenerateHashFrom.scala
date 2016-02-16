ddfa1aad2187124da34f911ba42a80e241841788
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-02-16 15:05:58 +0200

    Fix build

--- bloom-filter/src/main/scala/bloomfilter/CanGenerateHashFrom.scala
+++ bloom-filter/src/main/scala/bloomfilter/CanGenerateHashFrom.scala
@@ -1,6 +1,7 @@
 package bloomfilter
 
-import bloomfilter.MurmurHash3.LongPair
+import bloomfilter.hash.MurmurHash3
+import bloomfilter.hash.MurmurHash3.LongPair
 
 trait CanGenerateHashFrom[-From] {
   def generateHash(from: From): LongPair
@@ -9,18 +10,15 @@ trait CanGenerateHashFrom[-From] {
 object CanGenerateHashFrom {
 
   implicit object CanGenerateHashFromLong extends CanGenerateHashFrom[Long] {
-    override def generateHash(from: Long): LongPair = {
-      println("long")
-      new LongPair
-    }
+    override def generateHash(from: Long): LongPair = new LongPair
   }
 
   implicit object CanGenerateHashFromByteArray extends CanGenerateHashFrom[Array[Byte]] {
-    override def generateHash(from: Array[Byte]): LongPair = new LongPair
-  }
-
-  implicit object CanGenerateHashFromBoolean extends CanGenerateHashFrom[Boolean] {
-    override def generateHash(from: Boolean): LongPair = new LongPair
+    override def generateHash(from: Array[Byte]): LongPair = {
+      val pair = new LongPair
+      MurmurHash3.murmurhash3_x64_128(from, 0, from.length, 0, pair)
+      pair
+    }
   }
 
   implicit object CanGenerateHashFromNothing extends CanGenerateHashFrom[Nothing] {
