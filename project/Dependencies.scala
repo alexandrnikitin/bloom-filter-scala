464e320812e7e55cd92fa5c627d5efe2ba15ff36
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-02-19 13:31:00 +0200

    Add warning silencer plugin

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,10 +2,14 @@ import sbt.Keys._
 import sbt._
 
 object Dependencies {
+  private val silencer = "com.github.ghik" % "silencer-lib" % "0.3"
   private val scalatest = "org.scalatest" %% "scalatest" % "2.2.6" % "test;bt;e2e"
 
-  val tests = dependencies(scalatest)
-  val benchmark = dependencies(scalatest)
+  private val common = dependencies(silencer)
+
+  val bloomfilter = common
+  val tests = common ++ dependencies(scalatest)
+  val benchmark = common ++ dependencies(scalatest)
 
   private def dependencies(modules: ModuleID*): Seq[Setting[_]] = Seq(libraryDependencies ++= modules)
 }
--- project/Settings.scala
+++ project/Settings.scala
@@ -1,15 +1,20 @@
+import sbt._
 import sbt.Keys._
 
 object Settings {
 
   private lazy val build = Seq(
     scalaVersion := "2.11.7",
+
+    autoCompilerPlugins := true,
+    addCompilerPlugin("com.github.ghik" % "silencer-plugin" % "0.3"),
+
     scalacOptions ++= commonScalacOptions,
     organization := "com.github.alexandrnikitin"
   )
 
   lazy val root = build ++ Testing.settings ++ Publishing.noPublishSettings
-  lazy val bloomfilter = build ++ Testing.settings ++ Publishing.settings
+  lazy val bloomfilter = build ++ Testing.settings ++ Dependencies.bloomfilter ++ Publishing.settings
   lazy val tests = build ++ Testing.settings ++ Dependencies.tests
   lazy val benchmark = build ++ Testing.settings ++ Dependencies.benchmark
 
