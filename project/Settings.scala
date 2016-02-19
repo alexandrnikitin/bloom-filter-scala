26e967d6a4991acc2e6e1cde7ccad5057edaa35b
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-02-19 13:29:43 +0200

    Add scalac options

--- project/Settings.scala
+++ project/Settings.scala
@@ -4,6 +4,7 @@ object Settings {
 
   private lazy val build = Seq(
     scalaVersion := "2.11.7",
+    scalacOptions ++= commonScalacOptions,
     organization := "com.github.alexandrnikitin"
   )
 
@@ -11,5 +12,17 @@ object Settings {
   lazy val bloomfilter = build ++ Testing.settings ++ Publishing.settings
   lazy val tests = build ++ Testing.settings ++ Dependencies.tests
   lazy val benchmark = build ++ Testing.settings ++ Dependencies.benchmark
+
+  lazy val commonScalacOptions = Seq(
+    "-deprecation",
+    "-encoding", "UTF-8",
+    "-feature",
+    "-unchecked",
+    "-Xfatal-warnings",
+    "-Xlint:_",
+    "-Ywarn-dead-code",
+    "-Ywarn-numeric-widen",
+    "-Ywarn-value-discard"
+  )
 
 }
