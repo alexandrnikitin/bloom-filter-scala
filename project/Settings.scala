c60a9ed1fd5c9914a2361237cc91651ab42d3cff
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-02-26 22:44:25 +0200

    Divide scalac options into base and strict

--- project/Settings.scala
+++ project/Settings.scala
@@ -9,28 +9,34 @@ object Settings {
     autoCompilerPlugins := true,
     addCompilerPlugin("com.github.ghik" % "silencer-plugin" % "0.3"),
 
-    scalacOptions ++= commonScalacOptions,
+    scalacOptions ++= ScalacSettings.base,
     organization := "com.github.alexandrnikitin"
   )
 
   lazy val root = build ++ Testing.settings ++ Publishing.noPublishSettings
-  lazy val bloomfilter = build ++ Testing.settings ++ Dependencies.bloomfilter ++ Publishing.settings
+  lazy val bloomfilter = build ++ Testing.settings ++ Dependencies.bloomfilter ++ Publishing.settings ++ (scalacOptions ++= ScalacSettings.strict)
   lazy val sandbox = build ++ Testing.settings ++ Dependencies.sandbox
   lazy val tests = build ++ Testing.settings ++ Dependencies.tests
   lazy val benchmark = build ++ Testing.settings ++ Dependencies.benchmark
 
-  lazy val commonScalacOptions = Seq(
-    "-deprecation",
-    "-encoding", "UTF-8",
-    "-feature",
-    "-unchecked",
-    "-Xfatal-warnings",
-    "-Xlint",
-    "-Ywarn-unused",
-    "-Ywarn-unused-import",
-    "-Ywarn-dead-code",
-    "-Ywarn-numeric-widen",
-    "-Ywarn-value-discard"
-  )
+  object ScalacSettings {
+    val base = Seq(
+      "-deprecation",
+      "-encoding", "UTF-8",
+      "-feature",
+      "-unchecked",
+      "-optimise"
+    )
+
+    val strict = Seq(
+      "-Xfatal-warnings",
+      "-Xlint",
+      "-Ywarn-unused",
+      "-Ywarn-unused-import",
+      "-Ywarn-dead-code",
+      "-Ywarn-numeric-widen",
+      "-Ywarn-value-discard"
+    )
+  }
 
 }
