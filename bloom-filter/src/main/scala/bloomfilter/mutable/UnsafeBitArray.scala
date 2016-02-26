14a58762855c07472802bc19273bfcb22be3a077
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-02-26 22:49:46 +0200

    Initialize memory with zeroes

--- bloom-filter/src/main/scala/bloomfilter/mutable/UnsafeBitArray.scala
+++ bloom-filter/src/main/scala/bloomfilter/mutable/UnsafeBitArray.scala
@@ -5,6 +5,7 @@ import scala.concurrent.util.Unsafe.{instance => unsafe}
 class UnsafeBitArray(numberOfBits: Long) {
   private val indices = math.ceil(numberOfBits.toDouble / 64).toLong
   private val ptr = unsafe.allocateMemory(indices)
+  unsafe.setMemory(ptr, indices, 0.toByte)
 
   def get(index: Long): Boolean = {
     (unsafe.getLong(ptr + (index >>> 6)) & (1L << index)) != 0
