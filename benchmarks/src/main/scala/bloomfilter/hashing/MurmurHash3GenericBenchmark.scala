496c25c5b77e779bf1bbdb559932db0d60a2f56b
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-02-29 23:53:39 +0200

    MurmurHash3 generic benchmark

---
+++ benchmarks/src/main/scala/bloomfilter/hashing/MurmurHash3GenericBenchmark.scala
@@ -0,0 +1,19 @@
+package bloomfilter.hashing
+
+import org.openjdk.jmh.annotations.{Benchmark, Scope, State}
+
+@State(Scope.Benchmark)
+class MurmurHash3GenericBenchmark {
+
+  val key = Range(0, 64).map(_.toByte).toArray
+
+  @Benchmark
+  def scalaVersion() = {
+    MurmurHash3.murmurhash3_x64_128(key, 0, key.length, 0)
+  }
+
+  @Benchmark
+  def genericVersion() = {
+    MurmurHash3Generic.murmurhash3_x64_128(key, 0, key.length, 0)
+  }
+}
