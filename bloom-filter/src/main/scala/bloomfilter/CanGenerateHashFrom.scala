f3e2b0c5dd12ad7ec0d2f9c846d22ed8a91d1535
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-02-29 15:05:38 +0200

    CanGenerateHashFromString - no allocations :bow:

--- bloom-filter/src/main/scala/bloomfilter/CanGenerateHashFrom.scala
+++ bloom-filter/src/main/scala/bloomfilter/CanGenerateHashFrom.scala
@@ -1,17 +1,30 @@
 package bloomfilter
 
-import hashing.MurmurHash3
+import com.github.ghik.silencer.silent
+import hashing.MurmurHash3Generic
+
+import scala.concurrent.util.Unsafe.{instance => unsafe}
 
 trait CanGenerateHashFrom[-From] {
   def generateHash(from: From): Long
 }
 
-@com.github.ghik.silencer.silent
+@silent
 object CanGenerateHashFrom {
 
   implicit object CanGenerateHashFromLong extends CanGenerateHashFrom[Long] {
-    override def generateHash(from: Long): Long =
-      MurmurHash3.fmix64(from)
+    override def generateHash(from: Long): Long = from
+  }
+
+  implicit object CanGenerateHashFromString extends CanGenerateHashFrom[String] {
+
+    private val valueOffset = unsafe.objectFieldOffset(classOf[String].getDeclaredField("value"))
+    private val charBase = unsafe.arrayBaseOffset(classOf[Array[java.lang.Character]])
+
+    override def generateHash(from: String): Long = {
+      val value = unsafe.getObject(from, valueOffset).asInstanceOf[Array[Char]]
+      MurmurHash3Generic.murmurhash3_x64_64(value, charBase, from.length * 2, 0)
+    }
   }
 
 }
