9d4b6bb05f197e26a846c6a8f11b9c6b21e936bf
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-02-29 15:17:27 +0200

    Twitter's Bloom filter sucks :sob::sob::sob:

--- benchmarks/src/main/scala/bloomfilter/StringBenchmark.scala
+++ benchmarks/src/main/scala/bloomfilter/StringBenchmark.scala
@@ -2,6 +2,7 @@ package bloomfilter
 
 import bloomfilter.mutable.BloomFilter
 import org.openjdk.jmh.annotations.{Benchmark, Scope, State}
+import com.twitter.algebird.{BloomFilter => AlgebirdBloomFilter}
 
 @State(Scope.Benchmark)
 class StringBenchmark {
@@ -10,10 +11,16 @@ class StringBenchmark {
   private val falsePositiveRate = 0.01
 
   val myBF = BloomFilter[String](itemsExpected, falsePositiveRate)
+  val algebirdBF = AlgebirdBloomFilter(itemsExpected.toInt, falsePositiveRate, 0).create("first element to create BF")
 
   @Benchmark
   def my() = {
     myBF.add("Some string")
+  }
+
+  @Benchmark
+  def algebird() = {
+    algebirdBF.+("Some string")
   }
 
 }
