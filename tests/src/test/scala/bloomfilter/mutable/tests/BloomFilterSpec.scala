9e06ebc0e5548ab8087cdb6cb17529900bd3f470
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-04-03 22:02:14 +0300

    Make BloomFilterSpec generic

--- tests/src/test/scala/bloomfilter/mutable/tests/BloomFilterSpec.scala
+++ tests/src/test/scala/bloomfilter/mutable/tests/BloomFilterSpec.scala
@@ -1,15 +1,16 @@
 package bloomfilter.mutable.tests
 
+import bloomfilter.CanGenerateHashFrom
 import bloomfilter.mutable.BloomFilter
 import org.scalacheck.commands.Commands
-import org.scalacheck.{Gen, Prop, Properties}
+import org.scalacheck.{Arbitrary, Gen, Prop, Properties}
 
 class BloomFilterSpec extends Properties("BloomFilter") {
 
-  property("mightContain") = BloomFilterCommands.property()
+  property("mightContain") = new BloomFilterCommands[Long].property()
 
-  object BloomFilterCommands extends Commands {
-    type Sut = BloomFilter[Long]
+  class BloomFilterCommands[T: Arbitrary](implicit canGenerateHash: CanGenerateHashFrom[T]) extends Commands {
+    type Sut = BloomFilter[T]
 
     case class State(expectedItems: Long)
 
@@ -27,23 +28,23 @@ class BloomFilterSpec extends Properties("BloomFilter") {
       Gen.chooseNum[Long](1, Int.MaxValue).map(State)
 
     override def newSut(state: State): Sut =
-      BloomFilter[Long](state.expectedItems, 0.01)
+      BloomFilter[T](state.expectedItems, 0.01)
 
     def initialPreCondition(state: State): Boolean = true
 
     def genCommand(state: State): Gen[Command] =
       for {
-        item <- Gen.choose[Long](Long.MinValue, Long.MaxValue)
+        item <- Arbitrary.arbitrary[T]
       } yield commandSequence(AddItem(item), CheckItem(item))
 
-    case class AddItem(item: Long) extends UnitCommand {
+    case class AddItem(item: T) extends UnitCommand {
       def run(sut: Sut): Unit = sut.synchronized(sut.add(item))
       def nextState(state: State) = state
       def preCondition(state: State) = true
       def postCondition(state: State, success: Boolean) = success
     }
 
-    case class CheckItem(item: Long) extends SuccessCommand {
+    case class CheckItem(item: T) extends SuccessCommand {
       type Result = Boolean
       def run(sut: Sut): Boolean = sut.synchronized(sut.mightContain(item))
       def nextState(state: State) = state
