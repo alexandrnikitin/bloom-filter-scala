6d6dafc36d124679ce103a3be85df2f56f967971
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-04-03 20:49:28 +0300

    Introduce BloomFilter property spec

---
+++ tests/src/test/scala/bloomfilter/mutable/tests/BloomFilterSpec.scala
@@ -0,0 +1,60 @@
+package bloomfilter.mutable.tests
+
+import bloomfilter.mutable.BloomFilter
+import org.scalacheck.commands.Commands
+import org.scalacheck.{Gen, Prop, Properties}
+
+import scala.util.{Success, Try}
+
+class BloomFilterSpec extends Properties("BloomFilter") {
+
+  property("mightContain") = BloomFilterCommands.property()
+
+  object BloomFilterCommands extends Commands {
+    type Sut = BloomFilter[Long]
+
+    case class State(expectedItems: Long)
+
+    override def canCreateNewSut(
+        newState: State,
+        initSuts: Traversable[State],
+        runningSuts: Traversable[Sut]): Boolean = {
+      initSuts.isEmpty && runningSuts.isEmpty
+    }
+
+    override def destroySut(sut: Sut): Unit =
+      sut.dispose()
+
+    override def genInitialState: Gen[State] =
+      Gen.chooseNum[Long](1, Int.MaxValue).map(State)
+
+    override def newSut(state: State): Sut =
+      BloomFilter[Long](state.expectedItems, 0.01)
+
+    def initialPreCondition(state: State): Boolean = true
+
+    def genCommand(state: State): Gen[Command] =
+      for {
+        item <- Gen.choose[Long](Long.MinValue, Long.MaxValue)
+      } yield commandSequence(AddItem(item), CheckItem(item))
+
+    case class AddItem(item: Long) extends UnitCommand {
+      def run(sut: Sut): Unit = sut.synchronized(sut.add(item))
+      def nextState(state: State) = state
+      def preCondition(state: State) = true
+      def postCondition(state: State, success: Boolean) = success
+    }
+
+    case class CheckItem(item: Long) extends Command {
+      type Result = Boolean
+      def run(sut: Sut): Boolean = sut.synchronized(sut.mightContain(item))
+
+      def nextState(state: State) = state
+      def preCondition(state: State) = true
+      def postCondition(state: State, result: Try[Boolean]): Prop =
+        result == Success(true)
+    }
+
+  }
+
+}
