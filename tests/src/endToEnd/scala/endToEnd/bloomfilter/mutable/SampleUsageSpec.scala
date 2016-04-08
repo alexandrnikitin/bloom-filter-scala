708263e79c91d61d234397269884cfc406b84a2a
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-04-08 23:12:35 +0300

    Better End-to-end tests

---
+++ tests/src/endToEnd/scala/endToEnd/bloomfilter/mutable/SampleUsageSpec.scala
@@ -0,0 +1,20 @@
+package endToEnd.bloomfilter.mutable
+
+import bloomfilter.mutable.BloomFilter
+import org.scalatest.{FreeSpec, Matchers}
+
+class SampleUsageSpec extends FreeSpec with Matchers {
+  "Create, put and check " in {
+    val bloomFilter = BloomFilter[String](1000, 0.01)
+
+    bloomFilter.add("")
+    bloomFilter.add("Hello!")
+    bloomFilter.add("8f16c986824e40e7885a032ddd29a7d3")
+
+    bloomFilter.mightContain("") shouldBe true
+    bloomFilter.mightContain("Hello!") shouldBe true
+    bloomFilter.mightContain("8f16c986824e40e7885a032ddd29a7d3") shouldBe true
+
+    bloomFilter.dispose()
+  }
+}
