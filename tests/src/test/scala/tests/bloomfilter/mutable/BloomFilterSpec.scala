00295301fe85efe47f987c990a9ac65629f83036
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-04-08 23:06:47 +0300

    Better property tests

--- tests/src/test/scala/tests/bloomfilter/mutable/BloomFilterSpec.scala
+++ tests/src/test/scala/tests/bloomfilter/mutable/BloomFilterSpec.scala
@@ -2,32 +2,41 @@ package tests.bloomfilter.mutable
 
 import bloomfilter.CanGenerateHashFrom
 import bloomfilter.mutable.BloomFilter
+import org.scalacheck.Test.Parameters
 import org.scalacheck.commands.Commands
 import org.scalacheck.{Arbitrary, Gen, Prop, Properties}
 
 class BloomFilterSpec extends Properties("BloomFilter") {
 
-  property("mightContain Long") = new BloomFilterCommands[Long].property()
-  property("mightContain String") = new BloomFilterCommands[String].property()
-  property("mightContain Array[Byte]") = new BloomFilterCommands[Array[Byte]].property()
+  property("for Long") = new BloomFilterCommands[Long].property()
+  property("for String") = new BloomFilterCommands[String].property()
+  property("for Array[Byte]") = new BloomFilterCommands[Array[Byte]].property()
+
+
+  override def overrideParameters(p: Parameters): Parameters = {
+    super.overrideParameters(p).withMinSuccessfulTests(1000)
+    // TODO waiting for an issue to be solved https://github.com/rickynils/scalacheck/issues/233
+  }
 
   class BloomFilterCommands[T: Arbitrary](implicit canGenerateHash: CanGenerateHashFrom[T]) extends Commands {
     type Sut = BloomFilter[T]
 
-    case class State(expectedItems: Long)
+    case class State(expectedItems: Long, addedItems: Long)
 
     override def canCreateNewSut(
         newState: State,
         initSuts: Traversable[State],
         runningSuts: Traversable[Sut]): Boolean = {
-      initSuts.isEmpty && runningSuts.isEmpty
+      initSuts.isEmpty && runningSuts.isEmpty ||
+          newState.addedItems > newState.expectedItems ||
+          newState.addedItems > 100
     }
 
     override def destroySut(sut: Sut): Unit =
       sut.dispose()
 
     override def genInitialState: Gen[State] =
-      Gen.chooseNum[Long](1, Int.MaxValue).map(State)
+      Gen.chooseNum[Long](1, Int.MaxValue).map(State(_, 0))
 
     override def newSut(state: State): Sut =
       BloomFilter[T](state.expectedItems, 0.01)
@@ -41,7 +50,7 @@ class BloomFilterSpec extends Properties("BloomFilter") {
 
     case class AddItem(item: T) extends UnitCommand {
       def run(sut: Sut): Unit = sut.synchronized(sut.add(item))
-      def nextState(state: State) = state
+      def nextState(state: State) = state.copy(addedItems = state.addedItems + 1)
       def preCondition(state: State) = true
       def postCondition(state: State, success: Boolean) = success
     }
