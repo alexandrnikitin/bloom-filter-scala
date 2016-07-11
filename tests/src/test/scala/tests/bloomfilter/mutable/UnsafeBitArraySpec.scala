c77dd2b6bd2b566a17763caa92c3ed93378bb4fb
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-07-11 11:35:30 +0300

    Initial property test for UnsafeBitArray

--- tests/src/test/scala/tests/bloomfilter/mutable/UnsafeBitArraySpec.scala
+++ tests/src/test/scala/tests/bloomfilter/mutable/UnsafeBitArraySpec.scala
@@ -1,6 +1,7 @@
 package tests.bloomfilter.mutable
 
 import bloomfilter.mutable.UnsafeBitArray
+import org.scalacheck.Arbitrary.arbitrary
 import org.scalacheck.Prop.forAll
 import org.scalacheck.{Gen, Properties}
 
@@ -16,6 +17,48 @@ class UnsafeBitArraySpec extends Properties("UnsafeBitArray") {
     case (sut: UnsafeBitArray, index: Long) =>
       sut.set(index)
       sut.get(index)
+  }
+
+
+  def genListElems[A](max: Int)(implicit aGen: Gen[A]): Gen[List[A]] = {
+    Gen.posNum[Int].map(_ % max).flatMap(i => Gen.listOfN(i, aGen))
+  }
+
+  val numOfSetBits: Int = 100
+
+  val genUnion = for {
+    size <- Gen.oneOf[Long](1, 1000, Int.MaxValue, Int.MaxValue * 2)
+    array = new UnsafeBitArray(size)
+    indices <- genListElems[Long]((numOfSetBits % size).toInt)(arbitrary[Long])
+    thatArray = new UnsafeBitArray(size)
+    thatIndices <- genListElems[Long]((numOfSetBits % size).toInt)(arbitrary[Long])
+  } yield (array, indices, thatArray, thatIndices)
+
+  val genIntersection = for {
+    size <- Gen.oneOf[Long](1, 1000, Int.MaxValue, Int.MaxValue * 2)
+    array = new UnsafeBitArray(size)
+    indices <- genListElems[Long]((numOfSetBits % size).toInt)(arbitrary[Long])
+    thatArray = new UnsafeBitArray(size)
+    thatIndices <- genListElems[Long]((numOfSetBits % size).toInt)(arbitrary[Long])
+    commonIndices <- genListElems[Long]((numOfSetBits % size).toInt)(arbitrary[Long])
+  } yield (array, indices, thatArray, thatIndices, commonIndices)
+
+
+  property("|") = forAll(genUnion) {
+    case (array: UnsafeBitArray, indices: List[Long], thatArray: UnsafeBitArray, thatIndices: List[Long]) =>
+      indices.foreach(array.set)
+      thatIndices.foreach(thatArray.set)
+      val sut = array | thatArray
+      (indices ++ thatIndices).forall(sut.get)
+  }
+
+  property("&") = forAll(genIntersection) {
+    case (array: UnsafeBitArray, indices: List[Long], thatArray: UnsafeBitArray, thatIndices: List[Long], commonIndices: List[Long]) =>
+      indices.foreach(array.set)
+      thatIndices.foreach(thatArray.set)
+      commonIndices.foreach(x => { thatArray.set(x); thatArray.set(x) })
+      val sut = array & thatArray
+      commonIndices.forall(sut.get)
   }
 
 }
