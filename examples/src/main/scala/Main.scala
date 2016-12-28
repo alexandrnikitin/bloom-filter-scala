1493f2c611e287ab1466efe8fea765fe10aacb38
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-12-28 14:37:40 +0200

    Add repro

--- examples/src/main/scala/Main.scala
+++ examples/src/main/scala/Main.scala
@@ -2,10 +2,15 @@
 import bloomfilter.mutable.BloomFilter
 
 object Main extends App {
-  val expectedElements = 1000
-  val falsePositiveRate: Double = 0.1
+  val expectedElements = 1000000
+  val falsePositiveRate = 0.1
   val bf = BloomFilter[String](expectedElements, falsePositiveRate)
-  bf.add("some string")
-  bf.mightContain("some string")
-  bf.dispose()
-}
+
+  // Put an element
+  bf.add("lee")
+
+  // Check whether an element in a set
+  println(bf.mightContain("lee")) // return false
+
+  // Dispose the instance
+  bf.dispose()}
