d1f1e7a708840d2ec3f8aef99cbbee4ac00ccfbc
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2017-02-01 15:44:06 +0200

    UnsafeTable can find tags

--- bloom-filter/src/main/scala/bloomfilter/mutable/UnsafeTable.scala
+++ bloom-filter/src/main/scala/bloomfilter/mutable/UnsafeTable.scala
@@ -90,6 +90,46 @@ class UnsafeTable(val numberOfBuckets: Long, numberOfBitsPerItem: Int) {
     (false, oldtagToRet)
   }
 
+  private def haszero4(x: Long) = (((x)-0x1111L) & (~(x)) & 0x8888L) > 0
+  private def hasvalue4(x: Long, n: Long) = (haszero4((x) ^ (0x1111L * (n))))
+
+  private def haszero8(x: Long): Boolean = (((x)-0x01010101L) & (~(x)) & 0x80808080L) > 0
+  private def hasvalue8(x: Long, n: Long): Boolean = (haszero8((x) ^ (0x01010101L * (n))))
+
+  private def haszero12(x: Long): Boolean = (((x)-0x001001001001L) & (~(x)) & 0x800800800800L) > 0
+  private def hasvalue12(x: Long, n: Long): Boolean = (haszero12((x) ^ (0x001001001001L * (n))))
+
+  private def haszero16(x: Long): Boolean =  (((x)-0x0001000100010001L) & (~(x)) & 0x8000800080008000L) > 0
+  private def hasvalue16(x: Long, n: Long): Boolean = (haszero16((x) ^ (0x0001000100010001L * (n))))
+
+  def find(index: Long, index2: Long, tag: Long): Boolean = {
+    val p1 = ptr + index * bytesPerBucket
+    val p2 = ptr + index2 * bytesPerBucket
+    val v1 = unsafe.getLong(p1)
+    val v2 = unsafe.getLong(p2)
+
+    // caution: unaligned access & assuming little endian
+    if (numberOfBitsPerItem == 4 && tagsPerBucket == 4) {
+      hasvalue4(v1, tag) || hasvalue4(v2, tag)
+    } else if (numberOfBitsPerItem == 8 && tagsPerBucket == 4) {
+      hasvalue8(v1, tag) || hasvalue8(v2, tag)
+    } else if (numberOfBitsPerItem == 12 && tagsPerBucket == 4) {
+      hasvalue12(v1, tag) || hasvalue12(v2, tag)
+    } else if (numberOfBitsPerItem == 16 && tagsPerBucket == 4) {
+      hasvalue16(v1, tag) || hasvalue16(v2, tag)
+    } else {
+      var i = 0
+      while (i < tagsPerBucket) {
+        if ((readTag(index, i) == tag) || (readTag(index2, i) == tag)) {
+          return true
+        }
+        i+=1
+      }
+      false
+    }
+  }
+
+
 
   def dispose(): Unit = unsafe.freeMemory(ptr)
 }
