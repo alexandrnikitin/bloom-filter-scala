1ab5eae45dd10426aa2896dafddd198deff8b723
Eyal Farago <eyal@nrgene.com>
2017-03-16 16:54:38 +0200

    serialization_support: add a failing testscase for UnsafeBitArray.

--- tests/src/test/scala/tests/bloomfilter/mutable/UnsafeBitArraySpec.scala
+++ tests/src/test/scala/tests/bloomfilter/mutable/UnsafeBitArraySpec.scala
@@ -1,13 +1,20 @@
 package tests.bloomfilter.mutable
 
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
+
 import bloomfilter.mutable.UnsafeBitArray
 import org.scalacheck.Test.Parameters
 import org.scalacheck.commands.Commands
+import org.scalacheck.util.Buildable
 import org.scalacheck.{Gen, Prop, Properties}
+import org.scalatest.{Inspectors, Matchers}
 
-class UnsafeBitArraySpec extends Properties("UnsafeBitArray") {
+import scala.util.{Success, Try}
+
+class UnsafeBitArraySpec extends Properties("UnsafeBitArray") with Matchers with Inspectors {
 
   property("set & get") = new UnsafeBitArrayCommands().property()
+  property("serializable") = serializationProp
 
   override def overrideParameters(p: Parameters): Parameters = {
     super.overrideParameters(p).withMinSuccessfulTests(1000)
@@ -57,5 +64,47 @@ class UnsafeBitArraySpec extends Properties("UnsafeBitArray") {
 
   }
 
+  def serializationProp : Prop = {
+    case class State( sz : Int, included : Set[Int])
+    val genState = for{
+      sz <- Gen.posNum[Int]
+      included <- Gen.listOf( Gen.choose(0, sz))
+    } yield {
+      State(sz, included.toSet)
+    }
+
+    Prop.forAll(genState){
+      case State(sz, included) =>
+        val bits = new UnsafeBitArray(sz)
+        try{
+          for{
+            idx <- included
+          } {
+            bits set idx
+          }
+
+          val bos = new ByteArrayOutputStream()
+          val oos = new ObjectOutputStream(bos)
+          oos.writeObject(bits)
+          oos.close()
+          val bis = new ByteArrayInputStream(bos.toByteArray)
+          val ois = new ObjectInputStream(bis)
+          val deserialized = ois.readObject()
+
+          deserialized should not be null
+          deserialized should be ( a[UnsafeBitArray] )
+          val deserializedBits = deserialized.asInstanceOf[UnsafeBitArray]
+          try{
+            deserializedBits.numberOfBits should equal( bits.numberOfBits )
+            forAll(0l until bits.numberOfBits){ idx =>
+              bits.get(idx) should equal( deserializedBits.get(idx))
+            }
+          } finally {
+            deserializedBits.dispose()
+          }
+        } finally bits.dispose()
+        Prop.passed
+    }
+  }
 }
 
