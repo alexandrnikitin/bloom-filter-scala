5f162df0222de6005ca51238a780248cf7757bd7
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2020-04-22 21:32:02 -0700

    Update all deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 
 object Dependencies {
   private val scalatest = "org.scalatest" %% "scalatest" % "3.1.1" % "test;endToEnd"
-  private val scalacheck = "org.scalacheck" %% "scalacheck" % "1.14.0" % "test"
+  private val scalacheck = "org.scalacheck" %% "scalacheck" % "1.14.3" % "test"
   private val googleGuava = "com.google.guava" % "guava" % "19.0"
   private val googleFindbugs = "com.google.code.findbugs" % "jsr305" % "2.0.3" // needed by guava
-  private val breeze = "org.scalanlp" %% "breeze" % "0.12"
-  private val breezeNatives = "org.scalanlp" %% "breeze-natives" % "0.12"
-  private val algebird = "com.twitter" %% "algebird-core" % "0.11.0"
+  private val breeze = "org.scalanlp" %% "breeze" % "1.0"
+  private val breezeNatives = "org.scalanlp" %% "breeze-natives" % "1.0"
+  private val algebird = "com.twitter" %% "algebird-core" % "0.13.6"
   private val sketches = "com.yahoo.datasketches" % "sketches-core" % "0.3.2"
   private val chronicleBytes = "net.openhft" % "chronicle-bytes" % "1.2.3"
   private val allocationInstrumenter = "com.google.code.java-allocation-instrumenter" % "java-allocation-instrumenter" % "3.0.1"
